b3a0311ed55ba777c60ad34feff37d6b99082bd8
Boris Kolozsi <boris.kolozsi@gmail.com>
2019-09-25 15:48:30 +0200

    fixed dependencies

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "ImageRow", path: "Sources", dependencies: ["Eureka"]),
+        .target(name: "ImageRow", dependencies: ["Eureka"], path: "Sources"),
     ],
     swiftLanguageVersions: [.version("5")]
 )
